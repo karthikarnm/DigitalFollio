286d2aab936df6130ccab1251736348af7e0acba
Karthi <85386654+karthikarnm@users.noreply.github.com>
2023-10-05 12:39:10 +0530

    adjustment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,10 @@ import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
       <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0  xs:top-20 top-[170px] max-w-7xl mx-auto flex flex-row items-start gap-5 mb-5 `}>
         <div className='flex flex-col justify-center items-center mt-5' >
         <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-        <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className='w-1 sm:h-90 h-40 violet-gradient' />
         </div>
         <div>
           <h3 className={`${styles.heroHeadText} text-white`}>Hi, I'm 
@@ -23,7 +23,7 @@ const Hero = () => {
       <div className=' xs:overflow hidden w-full'>       
         <ComputersCanvas />
 </div>
-        <div className='absolute xs:bottom-20 bottom-fixed bottom-0 w-full flex justify-center items-center'>
+        <div className='absolute xs:bottom-20  bottom-[110px] w-full flex justify-center items-center'>
           <a href = '#about'>
             <div className=' w-[30px] h-[54px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 cursor-pointer '>
             <motion.dev
